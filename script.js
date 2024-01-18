2920daeb87ddfb421a6b08be7054140e4364d345
UnsecureGarden <133285705+UnsecureGarden@users.noreply.github.com>
2024-01-18 01:43:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var playButton = document.getElementById('playButton');
     var audioPlayer = document.getElementById('audioPlayer');
 
-    playButton.addEventListener('click', function () {
-        if (audioPlayer.paused) {
-            audioPlayer.play();
-        } else {
-            audioPlayer.pause();
-        }
+    // Ajout d'un écouteur d'événement pour masquer le bouton "Clique ici" lors du chargement de la page
+    audioPlayer.addEventListener('loadedmetadata', function () {
+        var playButton = document.getElementById('playButton');
+        playButton.style.display = 'none';
+    });
+
+    // Ajout d'un écouteur d'événement sur le bouton "play" du lecteur audio
+    audioPlayer.addEventListener('play', function () {
+        var playButton = document.getElementById('playButton');
+        playButton.style.display = 'none';  // Masquer le bouton lorsqu'il est en cours de lecture
+    });
+
+    // Ajout d'un écouteur d'événement sur le bouton "pause" du lecteur audio
+    audioPlayer.addEventListener('pause', function () {
+        var playButton = document.getElementById('playButton');
+        playButton.style.display = 'block';  // Afficher le bouton lors de la pause
     });
 });
